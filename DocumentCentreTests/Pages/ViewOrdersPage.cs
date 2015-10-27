a17ed20c7e3b706e7be064e54a24a8e3b15a5bf3
Milan Sobat <msobat@lbmx.com>
2015-10-27 14:22:28 -0400

    ViewOrders

---
+++ DocumentCentreTests/Pages/ViewOrdersPage.cs
@@ -0,0 +1,32 @@
+﻿using Gallio.Framework;
+using Gallio.Model;
+using MbUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Remote;
+using OpenQA.Selenium.Support.UI;
+using System;
+
+namespace DocumentCentreTests.Pages
+{
+    public class ViewOrdersPage
+    {
+        private IWebDriver driver;
+
+        /// <summary>
+        /// Class representing View Orders for Members
+        /// </summary>
+        /// <param name="driver">Main interface for testing, represents idealised web browser</param>
+        public ViewOrdersPage(IWebDriver driver)
+        {
+            this.driver = driver;
+
+            // check if on correct page
+            if (!"My Orders".Equals(driver.Title))
+            {
+                throw new NoSuchWindowException("View Orders page not found");
+            }
+        }
+
+        
+    }
+}
---
+++ DocumentCentreTests/Util/Constants.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DocumentCentreTests.Util
+{
+     /// <summary>contains constants used by the tests such as the user name and password for the sauce labs</summary>
+    internal static class Constants
+    {
+        /// <summary>name of the sauce labs account that will be used</summary>
+        internal const string SAUCE_LABS_ACCOUNT_NAME = "milan03";
+        /// <summary>account key for the sauce labs account that will be used</summary>
+        internal const string SAUCE_LABS_ACCOUNT_KEY = "ecb17003-7131-4183-8754-b0b8b66aa708";
+
+        /// <summary>member portal login info
+        internal const string MEM_PORTAL_USER = "test01dealer";
+        internal const string MEM_PORTAL_PASS = "imatestthis";
+
+        // NOTE:
+        // To change the maximum number of parallel tests edit DegreeOfParallelism in AssemblyInfo.cs
+    }
+    
+    
+}
