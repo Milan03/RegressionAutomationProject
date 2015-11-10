41f1cf6efc5bee5ac353ea67ea2923f8c57b75ce
Milan Sobat <msobat@lbmx.com>
2015-11-10 14:47:20 -0500

    Refactored FindElements to swtich statement

--- DocumentCentreTests/Util/HelperMethods.cs
+++ DocumentCentreTests/Util/HelperMethods.cs
@@ -9,47 +9,56 @@ using System;
 namespace DocumentCentreTests.Util
 {
     /// <summary>contains functional methods frequently used during test</summary>
-    internal class HelperMethods
+    internal static class HelperMethods
     {
         /// <summary>
-        /// Helper method for finding elements
+        /// Wrapper for finding elements
         /// </summary>
+        /// <param name="driver">Interface for testing</param>
+        /// <param name="type">Type to search by</param>
+        /// <param name="element">Value to search for</param>
+        /// <returns></returns>
         public static IWebElement FindElement(IWebDriver driver, string type, string element)
         {
-            if ("name".Equals(type))
+            IWebElement foundElement;
+            switch (type)
             {
-                var foundElement = driver.FindElement(By.Name(element));
-                return foundElement;
+                case "name":
+                    foundElement = driver.FindElement(By.Name(element));
+                    break;
+                case "id":
+                    foundElement = driver.FindElement(By.Id(element));
+                    break;
+                case "classname":
+                    foundElement = driver.FindElement(By.ClassName(element));
+                    break;
+                case "linktext":
+                    foundElement = driver.FindElement(By.LinkText(element));
+                    break;
+                case "tagname":
+                    foundElement = driver.FindElement(By.TagName(element));
+                    break;
+                case "css":
+                    foundElement = driver.FindElement(By.CssSelector(element));
+                    break;
+                case "xpath":
+                    foundElement = driver.FindElement(By.XPath(element));
+                    break;
+                default:
+                    throw new NoSuchElementException("Element not found.");
             }
-            else if ("id".Equals(type))
-            {
-                var foundElement = driver.FindElement(By.Id(element));
-                return foundElement;
-            }
-            else if ("classname".Equals(type))
-            {
-                var foundElement = driver.FindElement(By.ClassName(element));
-                return foundElement;
-            }
-            else if ("linktext".Equals(type))
-            {
-                var foundElement = driver.FindElement(By.LinkText(element));
-                return foundElement;
-            }
-            else if ("tagname".Equals(type))
-            {
-                var foundElement = driver.FindElement(By.TagName(element));
-                return foundElement;
-            }
-            else if ("css".Equals(type))
-            {
-                var foundElement = driver.FindElement(By.CssSelector(element));
-                return foundElement;
-            }
-            else
-                throw new NoSuchElementException("Element not found.");
+            return foundElement;
         }
 
+        /// <summary>
+        /// Wrapper for Selenium JavaScript executor
+        /// </summary>
+        /// <param name="driver">Main interface for testing</param>
+        /// <returns>Interface through which the user can execute JavaScript</returns>
+        public static IJavaScriptExecutor Scripts(this IWebDriver driver)
+        {
+            return (IJavaScriptExecutor)driver;
+        }
 
     }
 }
