6c93c7fae5bb70ca43bf304d851fa8c2879d76b0
Milan Sobat <msobat@lbmx.com>
2015-12-31 10:36:46 -0500

    added screenshotting capability

--- DocumentCentreTests/BaseDriverTest.cs
+++ DocumentCentreTests/BaseDriverTest.cs
@@ -5,6 +5,7 @@ using OpenQA.Selenium;
 using OpenQA.Selenium.Firefox;
 using System;
 using System.Collections.Generic;
+using System.Drawing.Imaging;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -20,6 +21,22 @@ namespace DocumentCentreTests
         {
             _driver = new FirefoxDriver();
             _driver.Navigate().GoToUrl("http://portal.test-web01.lbmx.com/login?redirect=%2f");
+        }
+
+        internal static void TakeScreenshot(string saveFileName)
+        {
+            try
+            {
+                Screenshot ss = ((ITakesScreenshot)_driver).GetScreenshot();
+                string n = string.Format(saveFileName +"-{0:yyyy-MM-dd_hh-mm-ss-tt}.jpg",
+                                            DateTime.Now);
+                ss.SaveAsFile(@"C:\logging\screenshots\" + n, System.Drawing.Imaging.ImageFormat.Jpeg);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+                throw;
+            }
         }
 
         Cleanup after = () => _driver.Quit();
