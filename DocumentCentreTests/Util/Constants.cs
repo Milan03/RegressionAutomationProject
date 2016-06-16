97b8967d2c5a8da5d3574682b49a8ec75c709567
Milan Sobat <msobat@lbmx.com>
2016-06-16 10:30:33 -0400

    Standardized XPath var naming

--- DocumentCentreTests/Util/Constants.cs
+++ DocumentCentreTests/Util/Constants.cs
@@ -29,15 +29,15 @@
         internal const string INVALID_PO = "asdf1234adfs";
 
         /// <summary>products page row identifiers
-        internal const string PROD_ROW_WRAPPERS = "//div[contains(@class, 'product-row-wrapper')]";
-        internal const string ALL_PROD_VARIANTS = "div[id^='variant_']";
-        internal const string PROD_VAR_QTYS_LOCATORS = "//div[contains(@id, 'variant')]/div[3]/span/span/input[2]";
-        internal const string ROW_TITLE_XPATH = "//tbody/tr/td[2]/div/a[1]";
-        internal const string ALL_QTY_BOXES = "//input[contains(@class,'k-formatted-value')]";
-        internal const string ROW_QTY_UP_XPATH = "//span[contains(@class,'k-i-arrow-n')]";
-        internal const string ROW_QTY_DOWN_XPATH = "//span[contains(@class,'k-i-arrow-s')]";
-        internal const string ROW_PRICE_XPATH = "//[contains(@class, 'price') and contains(@class ,'Hover')]";
-        internal const string ROW_UPDATE_XPATH = "//tbody/tr/td[2]/div[2]/div[2]/button";
+        internal const string PROD_ROW_WRAPPERS_XP = "//div[contains(@class, 'product-row-wrapper')]";
+        internal const string ALL_PROD_VARIANTS_XP = "div[id^='variant_']";
+        internal const string PROD_VAR_QTYS_XP = "//div[contains(@id, 'variant')]/div[3]/span/span/input[2]";
+        internal const string ROW_TITLE_XP = "//tbody/tr/td[2]/div/a[1]";
+        internal const string ALL_QTY_BOXES_XP = "//input[contains(@class,'k-formatted-value')]";
+        internal const string ROW_QTY_UP_XP = "//span[contains(@class,'k-i-arrow-n')]";
+        internal const string ROW_QTY_DOWN_XP = "//span[contains(@class,'k-i-arrow-s')]";
+        internal const string ROW_PRICE_XP = "//[contains(@class, 'price') and contains(@class ,'Hover')]";
+        internal const string ROW_UPDATE_XP = "//tbody/tr/td[2]/div[2]/div[2]/button";
 
         /// <summary>cart page row identifiers
         internal const string ITEM_DEL_BTN_XP = "//tbody/tr/td/div/button[3]";
@@ -48,23 +48,23 @@
         internal const string ITEM_QTY_XP = "//tbody/tr/td[8]";
         internal const string ITEM_TOTAL_XP = "//tbody/tr/td[9]";
 
-        /// <summary>XPath locators
-        internal const string XPATH_PO_LOCATOR = "id('ordersGrid')/div[2]/table/tbody/tr[1]/td[3]";
-        internal const string XPATH_DEL_ORDER = "//button[contains(@class, 'btn-delete')]";
-        internal const string XPATH_EDIT_ORDER = "//button[contains(@class, 'btn-copy')]";
-        internal const string XPATH_INFO_OK = "//button[.='OK']";
-        internal const string XPATH_ORDER_OK = "//div[contains(@class, 'modal-content')]/div[3]/div/div/button[2]";
-        internal const string XPATH_INFO_PRINT = "//button[.='Print']";
-        internal const string XPATH_INFO_EXPORT = "//button[.='Export']";
-        internal const string XPATH_INFO_EMAIL = "//button[.='Email']";
-        internal const string XPATH_INFO_FINISH = "//div[contains(@class, 'modal-content')]/div[3]/div/div/button[4]";
-        internal const string XPATH_ALERT_SUCCESS = "//div[contains(@class, 'toast-success')]";
-        internal const string XPATH_ALERT_FAILURE = "//div[contains(@class, 'toast-failure')]";
-        internal const string XPATH_CAT_LOCATOR = "//h1[contains(@class, 'catalog-tile-text') and contains (text(), 'Milan Automation Catalogue')]";
-        internal const string XPATH_REPORTS_LOCATOR = "//button[contains(text(), 'Reports')]";
-        internal const string XPATH_SHIPON_CAL = "id('orderHeaderContainer')/div[2]/div[1]/div/span/span/span/span";
-        internal const string XPATH_CANCELAFTER_CAL = "id('orderHeaderContainer')/div[2]/div[2]/div/span/span/span/span";
-        internal const string XPATH_MYCART_LINK = "//span[contains(text(), 'My Cart')]";
-        internal const string XPATH_DEL_ITEM_OK = "//div[contains(@class, 'modal-footer')]/div/div/button[2]";
+        /// <summary>purchase order locators
+        internal const string PO_LOCATOR_XP = "id('ordersGrid')/div[2]/table/tbody/tr[1]/td[3]";
+        internal const string DEL_ORDER_XP = "//button[contains(@class, 'btn-delete')]";
+        internal const string EDIT_ORDER_XP = "//button[contains(@class, 'btn-copy')]";
+        internal const string INFO_OK_XP = "//button[.='OK']";
+        internal const string ORDER_OK_XP = "//div[contains(@class, 'modal-content')]/div[3]/div/div/button[2]";
+        internal const string INFO_PRINT_XP = "//button[.='Print']";
+        internal const string INFO_EXPORT_XP = "//button[.='Export']";
+        internal const string INFO_EMAIL_XP = "//button[.='Email']";
+        internal const string INFO_FINISH_XP = "//div[contains(@class, 'modal-content')]/div[3]/div/div/button[4]";
+        internal const string ALERT_SUCCESS_XP = "//div[contains(@class, 'toast-success')]";
+        internal const string ALERT_FAILURE_XP = "//div[contains(@class, 'toast-failure')]";
+        internal const string CAT_LOCATOR_XP = "//h1[contains(@class, 'catalog-tile-text') and contains (text(), 'Milan Automation Catalogue')]";
+        internal const string REPORTS_LOCATOR_XP = "//button[contains(text(), 'Reports')]";
+        internal const string SHIPON_CAL_XP = "id('orderHeaderContainer')/div[2]/div[1]/div/span/span/span/span";
+        internal const string CANCELAFTER_CAL_XP = "id('orderHeaderContainer')/div[2]/div[2]/div/span/span/span/span";
+        internal const string MYCART_LINK_XP = "//span[contains(text(), 'My Cart')]";
+        internal const string DEL_ITEM_OK_XP = "//div[contains(@class, 'modal-footer')]/div/div/button[2]";
     }
 }
