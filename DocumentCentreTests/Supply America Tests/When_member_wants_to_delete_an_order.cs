dfbc9f2340a04b7a90c637a043c5a209d69f26c7
Milan Sobat <msobat@lbmx.com>
2016-06-20 10:04:23 -0400

    changed to delete catalogue order

--- DocumentCentreTests/Supply America Tests/When_member_wants_to_delete_an_order.cs
+++ DocumentCentreTests/Supply America Tests/When_member_wants_to_delete_an_order.cs
@@ -8,6 +8,10 @@ namespace DocumentCentreTests.Member_BD_Tests
     public class When_member_wants_to_delete_an_order : BaseDriverTest
     {
         static ViewOrdersPage _voPage;
+        static MemberHomePage _memHomePage;
+        static CataloguesPage _catPage;
+        static ProductsPage _prodPage;
+        static MyCartPage _cartPage;
         static HomePage _homePage;
 
         Establish context = () =>
@@ -16,25 +20,30 @@ namespace DocumentCentreTests.Member_BD_Tests
             _logger.Info("-- Member Order Delete Test Initiating --");
             LoginPage loginPage = new LoginPage(_driver, "member");
             _homePage = loginPage.LoginAs(Constants.SA_PORTAL_USER, Constants.SA_PORTAL_PASS);
-            _voPage = _homePage.NavigateToOrders("View Draft Orders");
-            _voPage.OrderType = Constants.ORDER_SEARCH_DRAFT;
-            _voPage.InitiateSearch();
-            _voPage.CheckFirstRow();
+            //_voPage = _homePage.NavigateToOrders("View Draft Orders");
+            //_voPage.OrderType = Constants.ORDER_SEARCH_DRAFT;
+            //_voPage.InitiateSearch();
+            //_voPage.CheckFirstRow();
+            _catPage = _homePage.NavigateToCatalogues();
+            _catPage.InputCatalogueName("milan");
+            _catPage.InitiateSearch();
+            _prodPage = _catPage.ChooseCatalogue("Milan Automation Catalogue");
+            _cartPage = _prodPage.NavigateToCart();
         };
 
-        Because of = () => _voPage.DeleteOrder();
+        Because of = () => _memHomePage = _cartPage.DeleteOrder();
 
         It should_delete_the_order = () =>
         {
-            if (_voPage.AlertSuccess.Equals(true))
+            if (_memHomePage.PageReached.Equals(true))
             {
                 _logger.Info("-- Member Order Delete Test: [PASSED] --");
-                _voPage.AlertSuccess.ShouldEqual(true);
+                _memHomePage.PageReached.ShouldEqual(true);
             }
             else
             {
                 _logger.Fatal("-- Member Order Delete Test: [FAILED] --");
-                _voPage.AlertSuccess.ShouldEqual(false);
+                _memHomePage.PageReached.ShouldEqual(false);
             }
         };
     }
