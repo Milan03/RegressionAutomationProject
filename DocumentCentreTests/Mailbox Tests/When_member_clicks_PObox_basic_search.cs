98a2d6ad6386e1d5e0a65505b20b525fc66c62a7
Milan Sobat <msobat@lbmx.com>
2016-06-23 13:30:31 -0400

    added 'PObox' to test name

---
+++ DocumentCentreTests/Mailbox Tests/When_member_clicks_PObox_basic_search.cs
@@ -0,0 +1,44 @@
+﻿using DocumentCentreTests.Pages;
+using DocumentCentreTests.Util;
+using Machine.Specifications;
+using System.Threading;
+
+namespace DocumentCentreTests.Mailbox_Tests
+{
+    [Subject(typeof(LoginPage))]
+    public class When_member_clicks_PObox_basic_search : BaseDriverTest
+    {
+        static SupplierHomePage _suppHomepage;
+        static POInboxPage _poInboxPage;
+
+        Establish context = () =>
+        {
+            LoadDriver();
+            _logger.Info("-- Member PO Mailbox Load Basic Search Test Initiating --");
+            LoginPage loginPage = new LoginPage(_driver, "supplier");
+            _suppHomepage = (SupplierHomePage)loginPage.LoginAs(Constants.SA_SUPPLIER_USER, Constants.SA_SUPPLIER_PASS);
+            _poInboxPage = (POInboxPage)_suppHomepage.NavigateToMailbox(Constants.VIEW_POS);
+        };
+
+        Because of = () =>
+        {
+            _poInboxPage.LoadAdvancedSearch();
+            Thread.Sleep(500);
+            _poInboxPage.LoadBasicSearch();
+        };
+
+        It should_load_basic_search = () =>
+        {
+            if (!_poInboxPage.BasicLoadSuccess)
+            {
+                _logger.Fatal("-- Member PO Mailbox Load Basic Search Test: [FAILED] --");
+                _poInboxPage.BasicLoadSuccess.ShouldBeTrue();
+            }
+            else
+            {
+                _logger.Info("-- Member PO Mailbox Load Basic Search Test: [PASSED] --");
+                _poInboxPage.BasicLoadSuccess.ShouldBeTrue();
+            }
+        };
+    }
+}
--- DocumentCentreTests/Mailbox Tests/When_member_loads_PObox_grid.cs
+++ DocumentCentreTests/Mailbox Tests/When_member_loads_PObox_grid.cs
@@ -1,12 +1,13 @@
 ﻿using DocumentCentreTests.Pages;
 using DocumentCentreTests.Util;
 using Machine.Specifications;
+using NCrunch.Framework;
 using System.Threading;
+
 
 namespace DocumentCentreTests.Mailbox_Tests
 {
-    [Subject(typeof(LoginPage))]
-    public class When_member_clicks_basic_search : BaseDriverTest
+    public class When_member_loads_PObox_grid : BaseDriverTest
     {
         static SupplierHomePage _suppHomepage;
         static POInboxPage _poInboxPage;
@@ -14,7 +15,7 @@ namespace DocumentCentreTests.Mailbox_Tests
         Establish context = () =>
         {
             LoadDriver();
-            _logger.Info("-- Member PO Mailbox Load Basic Search Test Initiating --");
+            _logger.Info("-- Member PO Mailbox Load Grid Test Initiating --");
             LoginPage loginPage = new LoginPage(_driver, "supplier");
             _suppHomepage = (SupplierHomePage)loginPage.LoginAs(Constants.SA_SUPPLIER_USER, Constants.SA_SUPPLIER_PASS);
             _poInboxPage = (POInboxPage)_suppHomepage.NavigateToMailbox(Constants.VIEW_POS);
@@ -22,22 +23,20 @@ namespace DocumentCentreTests.Mailbox_Tests
 
         Because of = () =>
         {
-            _poInboxPage.LoadAdvancedSearch();
-            Thread.Sleep(500);
-            _poInboxPage.LoadBasicSearch();
+            _poInboxPage.LoadGridRows();
         };
 
-        It should_load_basic_search = () =>
+        It should_load_all_grid_elements = () =>
         {
-            if (!_poInboxPage.BasicLoadSuccess)
+            if (!_poInboxPage.GridLoadSuccess)
             {
-                _logger.Fatal("-- Member PO Mailbox Load Basic Search Test: [FAILED] --");
-                _poInboxPage.BasicLoadSuccess.ShouldBeTrue();
+                _logger.Fatal("-- Member PO Mailbox Load Grid Test: [FAILED] --");
+                _poInboxPage.GridLoadSuccess.ShouldBeTrue();
             }
             else
             {
-                _logger.Info("-- Member PO Mailbox Load Basic Search Test: [PASSED] --");
-                _poInboxPage.BasicLoadSuccess.ShouldBeTrue();
+                _logger.Info("-- Member PO Mailbox Load Grid Test: [PASSED] --");
+                _poInboxPage.GridLoadSuccess.ShouldBeTrue();
             }
         };
     }
