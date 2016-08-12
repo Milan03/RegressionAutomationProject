92a48fbeb49f314fd179d516927f4c39842a83d0
Milan Sobat <msobat@lbmx.com>
2016-08-12 08:49:19 -0400

    Draft Edit Draft Order dev

---
+++ DocumentCentreTests/Functional Tests/Member/02 Catalogue/When_Drake_wants_to_edit_draft_po.cs
@@ -0,0 +1,31 @@
+﻿using DocumentCentreTests.Pages;
+using DocumentCentreTests.Util;
+using Machine.Specifications;
+using NCrunch.Framework;
+
+namespace DocumentCentreTests.Functional_Tests.Member._02_Catalogue
+{
+    public class When_Drake_wants_to_edit_draft_po : BaseDriverTest
+    {
+        static MemberHomePage _homePage;
+        static ViewOrdersPage _voPage;
+
+        Establish context = () =>
+        {
+            LoadDriver();
+            _logger.Info("-- Drake Member Edit Draft PO Test --");
+            LoginPage loginPage = new LoginPage(_driver, "member");
+            _homePage = (MemberHomePage)loginPage.LoginAs(Constants.DRAKE_MEMBER_USER, Constants.DRAKE_MEMBER_PASS);
+        };
+
+        Because of = () =>
+        {
+            _voPage = _homePage.NavigateToOrders("View Draft Orders", "Draft");
+        };
+
+        It should_save_the_edit = () =>
+        {
+
+        };
+    }
+}
