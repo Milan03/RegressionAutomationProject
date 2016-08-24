7819a4ce8604aaf177e6ffaa0f0e3e9bf61defd9
Milan Sobat <msobat@lbmx.com>
2016-08-24 10:23:22 -0400

    Drake Order Grid Delete test dev

--- DocumentCentreTests/Functional Tests/Member/02 Catalogue/When_Drake_member_deletes_po_from_grid.cs
+++ DocumentCentreTests/Functional Tests/Member/02 Catalogue/When_Drake_member_deletes_po_from_grid.cs
@@ -1,10 +1,11 @@
 ﻿using DocumentCentreTests.Pages;
 using DocumentCentreTests.Util;
 using Machine.Specifications;
+using NCrunch.Framework;
 
 namespace DocumentCentreTests.Functional_Tests.Member.Catalogue
 {
-    public class When_SA_member_deletes_an_order : BaseDriverTest
+    public class When_Drake_member_deletes_po_from_grid : BaseDriverTest
     {
         static ViewOrdersPage _voPage;
         static MemberHomePage _memHomePage;
@@ -12,24 +13,24 @@ namespace DocumentCentreTests.Functional_Tests.Member.Catalogue
         Establish context = () =>
         {
             LoadDriver();
-            _logger.Info("-- SA Order Grid Delete Test Initiating --");
+            _logger.Info("-- Drake Order Grid Delete Test Initiating --");
             LoginPage loginPage = new LoginPage(_driver, Constants.UserType.MEMBER);
-            _memHomePage = (MemberHomePage)loginPage.LoginAs(Constants.Affiliation.SA.MEMBER_USER, Constants.Affiliation.SA.MEMBER_PASS);
+            _memHomePage = (MemberHomePage)loginPage.LoginAs(Constants.Affiliation.Drake.MEMBER_USER, Constants.Affiliation.Drake.MEMBER_PASS);
             _voPage = _memHomePage.NavigateToOrders(Constants.Text.VIEW_DRAFT_ORDERS, Constants.OrderStatus.DRAFT);
             _voPage.OrderType = Constants.OrderStatus.DRAFT;
             _voPage.InitiateSearch();
             _voPage.CheckFirstRow();
         };
 
-        Because of = () => 
+        Because of = () =>
         {
             try
             {
                 _voPage.DeleteOrder();
             }
-            catch(System.Exception)
+            catch (System.Exception)
             {
-                _logger.Fatal("-- SA Order Grid Delete Test: [FAILED] --");
+                _logger.Fatal("-- Drake Order Grid Delete Test: [FAILED] --");
             }
         };
 
@@ -37,12 +38,12 @@ namespace DocumentCentreTests.Functional_Tests.Member.Catalogue
         {
             if (_memHomePage.PageReached)
             {
-                _logger.Info("-- SA Order Grid Delete Test: [PASSED] --");
-                _memHomePage.PageReached.ShouldBeTrue();
+                _logger.Info("-- Drake Order Grid Delete Test: [PASSED] --");
+                _memHomePage.PageReached.ShouldBeTrue(); ;
             }
             else
             {
-                _logger.Fatal("-- SA Order Grid Delete Test: [FAILED] --");
+                _logger.Fatal("-- Drake Order Grid Delete Test: [FAILED] --");
                 _memHomePage.PageReached.ShouldBeTrue();
             }
         };
